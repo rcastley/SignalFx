3b5846ddd9c2d4272ca5976e3c738975f2df24fb
Robert Castley <robert.castley@gmail.com>
2021-10-27 19:32:10 +0100

    Python3 fixes

--- custom_metrics_report_parser.py
+++ custom_metrics_report_parser.py
@@ -3,7 +3,7 @@
 import argparse
 import csv
 
-parser = argparse.ArgumentParser(description='SignalFx - Custom Metrics Report Parser')
+parser = argparse.ArgumentParser(description='Splunk O11y Cloud - Custom Metrics Report Parser')
 parser.add_argument('-c', '--category', help='1 (Host), 2 (Container), 3 (Custom), 4 (Hi-Res), 5 (Bundled)', default='3')
 parser.add_argument('-l', '--limit', help='Limit no. of metrics displayed in table', default=10000)
 parser.add_argument('-r', '--report', help='Custom Metric Report', required=True)
@@ -26,8 +26,8 @@ OKGREEN = '\033[92m'
 ENDC = '\033[0m'
 BOLD = '\033[1m'
     
-print BOLD + OKGREEN + "{: <80} {: >21}".format("\nSignalFx Metric (" + type + ")", "MTS") + ENDC
-print BOLD + OKGREEN + "{: <80} {: >20}".format("-" * 80, "-" * 9) + ENDC
+print(BOLD + OKGREEN + "{: <80} {: >21}".format("\nSplunk Metric (" + type + ")", "MTS") + ENDC)
+print(BOLD + OKGREEN + "{: <80} {: >20}".format("-" * 80, "-" * 9) + ENDC)
 
 with open(args['report']) as f:
     reader = csv.DictReader(f, delimiter='\t')
@@ -36,14 +36,12 @@ with open(args['report']) as f:
             metrics_list[row['Metric Name']] = int(row[type])
 
 total = 0
-
-res = sorted(metrics_list.items(), key=lambda (k,v): v, reverse=True)
-
+res = sorted(metrics_list.items(), key=lambda v: v[1], reverse=True)
 for r in res[:int(args['limit'])]:
     mts = "{:,}".format(r[1])
-    print "{: <80} {: >20}".format(r[0], mts)
+    print("{: <80} {: >20}".format(r[0], mts))
     total = total + int(r[1])
 
 total = "{:,}".format(total)
-print BOLD + OKGREEN + "{: <80} {: >20}".format("-" * 80, "-" * 9) + ENDC
-print BOLD + OKGREEN + "{: <80} {: >20}".format("Total MTS", total) + ENDC
+print(BOLD + OKGREEN + "{: <80} {: >20}".format("-" * 80, "-" * 9) + ENDC)
+print(BOLD + OKGREEN + "{: <80} {: >20}".format("Total MTS", total) + ENDC)
